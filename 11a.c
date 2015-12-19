92987eb4eb75b7bc351bc4b4624aa5ba33437a44
abecderic <abecderisch@web.de>
2015-12-19 21:56:08 +0100

    day 11

---
+++ 11a.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LENGTH 8
+
+void incrementString(char *string, int pos)
+{
+    if (string[pos] == 'z')
+	{
+		string[pos] = 'a';
+        if (pos > 0) incrementString(string, pos-1);
+	}
+	else
+		string[pos]++;
+}
+
+int hasStraight(char *string)
+{
+    int i;
+    for (i = 0; i < LENGTH; i++)
+	{
+        if (string[i-2]+1 == string[i-1] && string[i-1]+1 == string[i])
+			return 1;
+	}
+	return 0;
+}
+
+int onlyAllowed(char *string)
+{
+    int i;
+    for (i = 0; i < LENGTH; i++)
+	{
+		if (string[i] == 'i' || string[i] == 'o' || string[i] == 'l')
+			return 0;
+	}
+	return 1;
+}
+
+int hasDouble(char *string)
+{
+    int doubles = 0;
+	int i;
+	for (i = 1; i < LENGTH; i++)
+	{
+		if (string[i-1] == string[i])
+		{
+			doubles++;
+			i++;
+		}
+	}
+	return doubles >= 2;
+}
+
+int main(void)
+{
+    const char *input = "hxbxwxba";
+    char *string = malloc(strlen(input) + 1);
+    if (!string)
+	{
+		fprintf(stderr, "malloc fail with string\n");
+		exit(-1);
+	}
+    strcpy(string, input);
+
+	while (!hasStraight(string) || !onlyAllowed(string) || !hasDouble(string))
+	{
+		incrementString(string, LENGTH-1);
+	}
+    printf("%s\n", string);
+	return 0;
+}
